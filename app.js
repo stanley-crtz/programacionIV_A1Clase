1b34f1205d557c543a51b264c0fa8eac16919953
root <root@localhost.localdomain>
2020-02-20 15:59:46 -0600

    Convertidor

--- app.js
+++ app.js
@@ -79,40 +79,36 @@ document.addEventListener("DOMContentLoaded", e => {
 
         let $res = document.querySelector("#lblRespuesta");
 
+        let Convertidor;
+
         switch (Convertir){
 
             case "Moneda":
 
-                let Monedas = {"dolar":1, "euro":0.93, "quetzal":7.63, "lempira":24.9, "cordoba":34.19 };
-
-                $res.innerHTML = `Respuesta: ` + (Monedas[a]/Monedas[de]*cantidad).toFixed(2);
+                Convertidor = {"dolar":1, "euro":0.93, "quetzal":7.63, "lempira":24.9, "cordoba":34.19 };
 
                 break;
             case "Longitud":
 
-                let Longitud = { "metro":1, "kilometro":0.001, "centimetro":100, "pie":3.281, "pulgada":39.37};
-
-                $res.innerHTML = `Respuesta: ` + (Longitud[a]/Longitud[de]*cantidad).toFixed(3);
+                Convertidor = { "metro":1, "kilometro":0.001, "centimetro":100, "pie":3.281, "pulgada":39.37};
 
                 break;
 
             case "Almacenamiento":
 
-                let Almacenamiento = {"byte":125000, "kilobit":1000, "megabit":1, "gigabit":0.001, "terabit":0.000001};
-
-                $res.innerHTML = `Respuesta: ` + (Almacenamiento[a]/Almacenamiento[de]*cantidad).toFixed(6);
+                Convertidor = {"byte":125000, "kilobit":1000, "megabit":1, "gigabit":0.001, "terabit":0.000001};
 
                 break;
             
             case "Tiempo":
 
-                let Tiempo = {"milisegundo":60000, "segundo":60, "minuto":1, "hora":0.0166667, "dia":0.000694444 };
-
-                $res.innerHTML = `Respuesta: ` + (Tiempo[a]/Tiempo[de]*cantidad).toFixed(2);
+                Convertidor = {"milisegundo":60000, "segundo":60, "minuto":1, "hora":0.0166667, "dia":0.000694444 };
 
                 break;
 
         }
+
+        $res.innerHTML = `Respuesta: ` + (Convertidor[a]/Convertidor[de]*cantidad).toFixed(2);
 
     });
 
