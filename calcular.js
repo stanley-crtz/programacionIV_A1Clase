8fd50e3bbd97569c069abd78b4fb8bc34088e48b
root <root@localhost.localdomain>
2020-03-03 20:35:29 -0600

    Empezando

---
+++ calcular.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", e => {
+
+    const form = document.querySelector("#frmOperacion");
+
+    form.addEventListener("submit", event => {
+
+        let Opciones = document.querySelector("Operacion").value;
+
+    })
+
+})
